05f8e3df4ec4b19100e7e964196c8b9dea6312da
Gina Trapani <ginatrapani@gmail.com>
2017-10-15 08:10:39 -0400

    feat: use react-snapshot for pre-rendering

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import { render } from "react-snapshot";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(<App />, document.getElementById("root"));
 registerServiceWorker();
